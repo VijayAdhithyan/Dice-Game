1125308f8532d6336d01596fb4e7f3ab7e37f1ce
VijayAdhithyan <vijayadhithyan14@gmail.com>
2024-02-28 13:35:20 +0530

    Updated page functions

--- script.js
+++ script.js
@@ -1,23 +1,87 @@
-const div = document.createElement("div");
-div.innerHTML = `<div style = "display: flex; flex-direction: column; align-item: center;margin-top: 70px;row-gap:10px">
-<p style = "text-align: center; font-size : 18px;color:green">Let's Play Dice</p>
-<p style = "text-align: center; font-size : 30px">Player To Play</p>
-<div style="display:flex; align-item: center; justify-content: center; column-gap : 200px;margin-top:50px">
-    <div>
-        <p style= "text-align:center;font-size:24px">Player-1</p>
-        <p style= "text-align:center;font-size:28px"><b>0</b></p>
-        <p style="cursor:pointer;padding:10px 20px; background-color: green;color:#fff;text-align:center">Roll Now</p>
-    </div>
-    <div style="margin-top:50px">
-        <img src="./Images/num-1.png" width=130/>
-        <p style="cursor:pointer;margin-top:70px;padding:6px 0px; background-color:pink;text-align:center">Reset</p>
-    </div>
-    <div>
-        <p style= "text-align:center; font-size:24px">Player-2</p>
-        <p style= "text-align:center;font-size:28px"><b>0</b></p>
-        <p style="cursor:pointer;text-align:center;padding:10px 20px; background-color: #555;color:#fff">Roll Now</p>
-    </div>
-</div>
-</div>`;
+let player1Score = 0;
+let player2Score = 0;
 
-document.body.append(div)
+let player1 = true;
+let player2 = false;
+
+const playerName = document.getElementById("player-name");
+const player1ScoreUpdate = document.getElementById("player-1-score");
+const player2ScoreUpdate = document.getElementById("player-2-score");
+const player1Btn = document.getElementById("player-1-btn");
+const player2Btn = document.getElementById("player-2-btn");
+const resetBtn = document.getElementById("reset-btn");
+const diceImg = document.getElementById("dice-img");
+
+player1Btn.addEventListener("click", player1Turn);
+player2Btn.addEventListener("click", player2Turn);
+resetBtn.addEventListener("click", resetGame);
+
+// player 1 playing function
+function player1Turn() {
+  if (player1) {
+    let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    let res = "dice" + randomNumber1 + ".png";
+    let res2 = "./images/" + res;
+    let image1 = document.querySelectorAll("img")[0];
+    image1.setAttribute("src", res2);
+    player1Score += randomNumber1;
+    player1ScoreUpdate.textContent = player1Score;
+    if (player1Score >= 10) {
+      player2 = false;
+      player1 = false;
+      playerName.textContent = "Player-1 win";
+      player1Btn.classList.remove("active");
+      player2Btn.classList.remove("active");
+    } else {
+      player1 = false;
+      player2 = true;
+      playerName.textContent = "Player-2 to play";
+      player1Btn.classList.remove("active");
+      player2Btn.classList.add("active");
+    }
+  }
+}
+
+// player 2 playing function
+function player2Turn() {
+  if (player2) {
+    let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    let res = "dice" + randomNumber2 + ".png";
+    let res2 = "./images/" + res;
+    let image1 = document.querySelectorAll("img")[0];
+    image1.setAttribute("src", res2);
+    player2Score += randomNumber2;
+    player2ScoreUpdate.textContent = player2Score;
+    if (player2Score >= 10) {
+      player1 = false;
+      player2 = false;
+      playerName.textContent = "Player-2 win";
+      player1Btn.classList.remove("active");
+      player2Btn.classList.remove("active");
+    } else {
+      player1 = true;
+      player2 = false;
+      playerName.textContent = "Player-1 to play";
+      player2Btn.classList.remove("active");
+      player1Btn.classList.add("active");
+    }
+  }
+}
+
+// Game reset or restart function
+function resetGame() {
+  playerName.textContent = "Player-1 to play";
+  player1Score = 0;
+  player2Score = 0;
+  player1ScoreUpdate.textContent = 0;
+  player2ScoreUpdate.textContent = 0;
+  player2Btn.classList.remove("active");
+  player1Btn.classList.add("active");
+  let diceStart = "./images/dice1.png";
+  let image2 = document.querySelectorAll("img")[0];
+  image2.setAttribute("src", diceStart);
+  player1 = false;
+  player2 = true;
+  player1Btn.classList.remove("active");
+  player2Btn.classList.add("active");
+}
